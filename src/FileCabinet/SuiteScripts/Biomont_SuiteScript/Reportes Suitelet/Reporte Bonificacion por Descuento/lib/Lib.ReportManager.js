208d74b2111a6ebb4ee8c2b128d79523a10a18bc
Ernesto <jorge.cywdt@gmail.com>
2023-12-08 13:08:44 -0500

    Cambios realizados por Victor Robles

---
+++ src/FileCabinet/SuiteScripts/Biomont_SuiteScript/Reportes Suitelet/Reporte Bonificacion por Descuento/lib/Lib.ReportManager.js
@@ -0,0 +1,16 @@
+/**
+ * @NApiVersion 2.1
+ */
+define([
+    './reports/Class.BonificacionDescuento',
+],
+
+    function (
+        BonificacionDescuento
+    ) {
+
+        return {
+            BonificacionDescuento
+        }
+
+    });
